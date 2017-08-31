172d99f78bb8244a80ad61b625d1eef8bb2c2ba9
clmKnights <31505827+clmKnights@users.noreply.github.com>
2017-08-31 16:23:57 +0530

    Add files via upload
    patch-0

---
+++ MicroCLM/src/main/java/com/bmc/innov8/cloud/OutlookClmApplication.java
@@ -0,0 +1,12 @@
+package com.bmc.innov8.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//this is main app
+@SpringBootApplication
+public class OutlookClmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutlookClmApplication.class, args);
+	}
+}
